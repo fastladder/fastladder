2343f8b54aa4beb17665164ea01837cebb9ed11a
Kensuke Nagae <kyanny@gmail.com>
2013-03-16 01:20:01 +0900

    Add sign up story spec

--- spec/requests/signup_stories_spec.rb
+++ spec/requests/signup_stories_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe "SignupStories" do
-  describe "GET /signup_stories" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get signup_stories_index_path
-      response.status.should be(200)
-    end
+  it "sign up as a first member" do
+    expect {
+      visit "/"
+      fill_in 'member_username', with: 'bulkneets'
+      fill_in 'member_password', with: 'mala'
+      fill_in 'member_password_confirmation', with: 'mala'
+      click_on 'Sign Up'
+
+      # expect(page.body).to match(/Thanks for signing up!/) # TODO: reader#index rendered without layout (notice flash does not rendered)
+    }.to change {
+      Member.count
+    }.by(1)
   end
 end
