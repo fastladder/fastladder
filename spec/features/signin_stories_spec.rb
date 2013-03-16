5723348eaf96dd8de4e082df7c8c0cf43f5ebaf2
Kohei Hasegawa <ameutau@gmail.com>
2013-03-17 04:08:55 +0900

    add signin/signout feature spec
    * rename requests to features (it become default from capybara2.0)
     * refs: http://alindeman.github.com/2012/11/11/rspec-rails-and-capybara-2.0-what-you-need-to-know.html

---
+++ spec/features/signin_stories_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "SigninStories" do
+  subject { page }
+
+  let(:member) {
+    Factory(:member, password: 'mala', password_confirmation: 'mala')
+  }
+
+  context "when sign in as a member" do
+    before { login_as(member) }
+
+    it {
+      expect(current_path).to be == '/reader/'
+      expect(find('#welcome')).to have_content member.username
+    }
+  end
+end
