afbf9554359c013dbae6808f6fed2ce9bf59ff89
Kensuke Nagae <kyanny@gmail.com>
2013-03-17 02:40:26 +0900

    Add test of SubscribeController#confirm, stubbed Rfeedfinder.feeds

--- spec/controllers/subscribe_controller_spec.rb
+++ spec/controllers/subscribe_controller_spec.rb
@@ -6,13 +6,36 @@ describe SubscribeController do
   end
 
   describe 'GET /confirm' do
-    before do
-      Rfeedfinder.stub(:feeds).and_return(['http://feeds.feedburner.com/mala/blog/'])
+    context 'feed exists' do
+      before do
+        Rfeedfinder.stub(:feeds).and_return(['http://feeds.feedburner.com/mala/blog/'])
+      end
+
+      it 'renders confirm.html.erb' do
+        get :confirm, { url: 'http://la.ma.la/blog/' }, { member_id: @member.id }
+        expect(response).to render_template('confirm')
+      end
+
+      it 'assigns @feeds' do
+        get :confirm, { url: 'http://la.ma.la/blog/' }, { member_id: @member.id }
+        expect(assigns(:feeds)).to be_a_kind_of(Array)
+      end
     end
 
-    it 'renders confirm.html.erb' do
-      get :confirm, { url: 'http://la.ma.la/blog/' }, { member_id: @member.id }
-      expect(response).to render_template('confirm')
+    context 'no feeds' do
+      before do
+        Rfeedfinder.stub(:feeds).and_return([])
+      end
+
+      it 'redirect to index' do
+        get :confirm, { url: 'http://localhost/' }, { member_id: @member.id }
+        expect(response).to redirect_to('/subscribe')
+      end
+
+      it 'shows flash notice' do
+        get :confirm, { url: 'http://localhost/' }, { member_id: @member.id }
+        expect(flash[:notice]).to eq("please check URL")
+      end
     end
   end
 end
