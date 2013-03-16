549f1f7fc2f7d2453e9f0ce5a2954a8808dd39f9
Kensuke Nagae <kyanny@gmail.com>
2013-03-17 02:51:02 +0900

    Remove lib/fastladder/feedfinder.rb
    It seems only exists reason for monkey patching Rfeedfinder.open_doc method.

--- lib/fastladder/feedfinder.rb
+++
@@ -1,7 +0,0 @@
-require "rfeedfinder"
-
-def Rfeedfinder.open_doc(link, options = {})
-  html_body = Fastladder::simple_fetch(link.to_s)
-  return nil unless html_body
-  Hpricot(html_body, :xml => true)
-end
