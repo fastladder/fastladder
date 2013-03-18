ed81be82f0164184028acd235a5d359cb8e17d0a
Yamagishi Kazutoshi <ykzts@desire.sh>
2013-03-19 01:40:30 +0900

    Remove loadJS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def loadJS(*filename)
-    filename.map {|str|
-      javascript_include_tag str
-    }.join("\n")
-  end
-
   def disp_users(num)
     "#{num} #{(num > 1 ? "users" : "user")}"
   end
