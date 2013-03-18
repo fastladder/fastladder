269421a1e05db69abdd333480d47c010c6ee34bf
Kensuke Nagae <kyanny@gmail.com>
2013-03-19 02:28:42 +0900

    Write characterization test for ApplicationController#login

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,37 @@
 require 'spec_helper'
 
 describe ApplicationController do
+  describe '#login' do
+    controller do
+      before_filter :login
+
+      def index
+        render nothing: true
+      end
+    end
+
+    let(:member) { FactoryGirl.create(:member, password: 'mala', password_confirmation: 'mala') }
+
+    context 'Member exists' do
+      it 'renders 200' do
+        get :index, { }, { member_id: member.id }
+        expect(response).to be_success
+      end
+
+      it 'assigns @member' do
+        get :index, { }, { member_id: member.id }
+        expect(assigns(:member)).to be
+      end
+    end
+
+    context 'Member not exists' do
+      it 'redirects to login_path' do
+        get :index, { }, { }
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+
   describe '.url_from_path' do
     it 'should extract URL from request-path' do
       request = double(:request)
