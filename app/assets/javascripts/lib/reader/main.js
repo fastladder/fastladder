2f801023938ba69b2d9b2bad654e2f84d2138724
Koutarou Chikuba <miz404@gmail.com>
2013-03-20 00:35:29 +0900

    extract hotkey_manager and updater from main

--- app/assets/javascripts/lib/reader/main.js
+++ app/assets/javascripts/lib/reader/main.js
@@ -1,26 +1,8 @@
-//API Key
-// (function(){
-// 	// API
-// 	LDR.API.StickyQuery = { ApiKey: ApiKey };
-// 	function getApiKey(){
-// 		var ck = new Cookie().parse();
-// 		for(var key in ck){
-// 			if(/_sid/.test(key)){
-// 				return ck[key]
-// 			}
-// 		}
-// 	}
-// 	if(/^\[/.test(ApiKey)){
-// 		LDR.API.StickyQuery = { ApiKey: getApiKey() };
-// 	}
-// }).call(LDR);
-
 window.onload   = init;
 window.onresize = function(){LDR.invoke_hook('WINDOW_RESIZE')};
 
 //TODO move to local var
 var FlatMenu = LDR.FlatMenu;
-
 
 /*
  State
@@ -79,17 +61,6 @@ function typecast_config(obj){
 	return obj;
 }
 
-
-function debugPrint(text){
-	var wd = debugPrint.wd || (debugPrint.wd = window.open());
-	try{
-		wd.document.write(text);
-		wd.document.close();
-	}catch(e){
-		debugPrint.wd = null;
-		debugPrint(text);
-	}
-}
 // formをAjax化する
 function ajaxize(element, callback){
 	element = $(element);
@@ -117,142 +88,6 @@ function ajaxize(element, callback){
 	addEvent(element, "submit", onsubmit);
 	element.submit = onsubmit;
 }
-
-function print_error(t){
-	var options = {scope: "error_message/" + t};
-	$("error_title").innerHTML = I18n.t('title', options);
-	$("error_body").innerHTML =  I18n.t('body', options);
-}
-/*
- DHTML Functions
-*/
-updater("help_window", function(){
-	if(State.help_show){
-		Element.show("help_window");
-		this.innerHTML = State.help_message;
-		var el   = State.help_snap;
-		var pos  = Position.cumulativeOffset(el);
-		var left = pos[0] + el.offsetWidth + 10;
-		var top  = pos[1] - 8;
-		DOM.move(this, left,top);
-	} else {
-		Element.hide(this);
-	}
-});
-
-//updaters
-(function(){
-	var ld_check = function(){
-		var c = document.cookie;
-		return (c.indexOf(".LUID") != -1) || (c.indexOf(".LL") != -1);
-	};
-	updater("error_window",function(){
-		Element.show(this);
-		centering(this,0,50);
-		if(!ld_check()){
-			print_error("login");
-		} else if(typeof _XMLHttpRequest == "undefined"){
-			print_error("xmlhttp");
-		} else {
-			print_error("busy");
-		}
-	});
-
-	updater("mode_text_view",function(){
-		this.innerHTML = I18n.t(Config.view_mode);
-	});
-	updater("mode_text_sort",function(){
-		this.innerHTML = I18n.t(Config.sort_mode);
-	});
-	/* navi */
-	updater("right_bottom_navi", print_navi);
-	updater("right_top_navi", print_navi);
-	updater("scroll_offset",function(){
-		this.innerHTML = State.scroll_offset + "/"
-	});
-
-	updater("folder_label",function(){
-		var item = subs_item(State.now_reading);
-		this.innerHTML = [
-			(item.folder ? item.folder.ry(8,"...") : I18n.t('Uncategolized')),
-			'<img src="/img/icon/tri_d.gif">'
-		].join("");
-	});
-
-	/* 未読件数合計 */
-	updater("total_unread_count", function(){
-		var count = subs.model.get_unread_count();
-		// var feed_count = subs.model.get_unread_feeds().length;
-		var feed_count = subs.model.get_unread_feeds_count();
-		var param = {
-			count : count || "0",
-			feed_count : feed_count || "0"
-		};
-		if(param.count < 0) return;
-		if(State.load_progress){
-			addClass(this, "progress")
-		} else {
-			removeClass(this, "progress")
-		}
-		var tmpl = I18n.t('unread_count_tmpl');
-		var tmpl_title = I18n.t('unread_count_title_tmpl');
-		this.innerHTML = tmpl.fill(param);
-		if(!State.guest_mode){
-			document.title = tmpl_title.fill(param);
-		}
-	});
-
-	updater("keybind_table", function(){
-		this.innerHTML = format_keybind();
-		var h = State.keyhelp_more ? this.offsetHeight + 65 + "px" : "150px";
-		$("keyhelp").style.height = h;
-	});
-	updater("feed_next", function(){
-		this.className = (!State.has_next) ? "disable" : "";
-		update("feed_paging_next");
-	});
-	updater("feed_prev", function(){
-		this.className = (State.viewrange.start == 0) ? "disable" : "";
-		update("feed_paging_prev");
-	});
-
-	updater("feed_paging_next", function(){
-		this.className = (!State.has_next) ? "disable" : "";
-	});
-	updater("feed_paging_prev", function(){
-		this.className = (State.viewrange.start == 0) ? "disable" : "";
-	});
-
-	updater("myfeed_tab", function(){
-		this.style.borderColor = State.show_left
-		 ? '#a5c5ff white white white'
-		 : 'white white #a5c5ff white';
-	}._try());
-
-	updater("show_all_button", function(){
-		var style = {
-			active : {
-				border : "1px solid #fff",
-				backgroundColor : "#d4d0c8",
-				borderColor : "gray white white gray"
-			},
-			inactive : {
-				border : "1px solid #f5f5f5",
-				backgroundColor : "#f5f5f5"
-			}
-		};
-		setStyle(this, style[Config.show_all ? "inactive" : "active"] );
-	});
-	updater("reload_button", function(){
-		var img_path = State.subs_reloading ? '/img/icon/reload_anime.gif' : '/img/icon/reload.gif';
-		var cursor   = State.subs_reloading ? 'wait' : 'pointer';
-		setStyle(this, {
-			backgroundImage: 'url('+img_path+')',
-			cursor: cursor
-		});
-	});
-
-}).call(LDR);
 
 /*
  Ajax and Ahah
@@ -499,73 +334,6 @@ function IME_off(msg){
 }
 
 
-(function(){
-	function show_error(){
-		State.show_error = true;
-		update("error_window")
-	}
-
-	function hide_error(){
-		State.show_error = false;
-		Element.hide("error_window");
-	}
-
-	LDR.setup_hotkey = function(){
-		Keybind = new HotKey(null, "reader");
-		Keybind.globalCallback = function(){
-			State.LastUserAction = new Date;
-			if(State.show_error) hide_error();
-		};
-		var keyconfig = [];
-		each(LDR.KeyConfig, function(value,key){
-			keyconfig.push([
-				value, Control[key]
-			])
-		});
-		if(browser.isWin && browser.isFirefox){
-			keyconfig.push([
-				"IME",
-				function(){
-					if(this.lastInvoke == "IME"){
-						IME_off(true);
-					}
-				}
-			]);
-		}
-		keyconfig.forEach(function(a){
-			Keybind.add(a[0],a[1])
-		});
-
-		// 入力フォームでキー操作を効かせる
-		Keybind.allow = /finder/i;
-		Keybind.filter = hotkey_filter;
-
-		// for safari
-		if(browser.isKHTML){
-			Keybind.add("up", Event.stop);
-			Keybind.add("down", Event.stop);
-		}
-	}
-}).call(LDR);
-
-function print_navi(){
-	var next_id = get_next();
-	var prev_id = get_prev();
-	var buf = [];
-	if(prev_id){
-		buf.push([
-			'<div class="button prev" onclick="Control.read_prev_subs()">',
-			'<span class="navi_text">≪ [[title]]</span></div>'
-		].join("").fill( subs_item(prev_id) ))
-	}
-	if(next_id){
-		buf.push([
-			'<div class="next button" onclick="Control.read_next_subs()">',
-			'<span class="navi_text">[[title]] ≫</span></div>'
-		].join("").fill( subs_item(next_id) ))
-	}
-	this.innerHTML = buf.join("");
-};
 
 
 function getStyle(element, style){
--- app/assets/javascripts/reader.js
+++ app/assets/javascripts/reader.js
@@ -3,6 +3,8 @@
 //= require lib/reader/pref
 //= require lib/reader/event_hook
 //= require lib/reader/event_trigger
+//= require lib/reader/hotkey_manager
+//= require lib/reader/updater
 //= require lib/reader/main
 //= require lib/reader/manage
 //= require lib/reader/widgets_en
