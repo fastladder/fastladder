15e1a9f43f5558a7e840e90cce8c12c19b1fced4
Koutarou Chikuba <miz404@gmail.com>
2013-03-20 00:18:07 +0900

    extract event_hook.js from main.js

--- app/assets/javascripts/reader.js
+++ app/assets/javascripts/reader.js
@@ -1,6 +1,7 @@
 //= require application
 //= require lib/queue
 //= require lib/reader/pref
+//= require lib/reader/event_hook
 //= require lib/reader/main
 //= require lib/reader/manage
 //= require lib/reader/widgets_en
