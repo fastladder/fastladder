94accfa08f40f142514a4f667c6fa4a0e2a2be6a
Koutarou Chikuba <miz404@gmail.com>
2013-03-19 23:27:30 +0900

    enclose Hook and EventTrigger

--- app/assets/javascripts/lib/reader/main.js
+++ app/assets/javascripts/lib/reader/main.js
@@ -14,6 +14,57 @@
 	}
 }).call(LDR);
 
+// Hook
+(function(){
+	/*
+	 Hook
+	*/
+	var Hook = Class.create();
+	Hook.extend({
+		initialize: function(){
+			this.callbacks = [];
+		},
+		isHook: true,
+		add: function(f){
+			this.callbacks.push(f)
+		},
+		exec: function(){
+			var args = arguments;
+			this.callbacks.forEach(function(f){
+				isFunction(f) && f.apply(null,args)
+			})
+		},
+		clear: function(){
+			this.callbacks = []
+		}
+	});
+
+	LDR.EventTrigger = Class.create();
+	LDR.EventTrigger.extend({
+		initialize: function(){
+			var points = Array.from(arguments);
+			var triggers = {};
+			points.forEach(function(name){
+				var hook_name = name.toLowerCase();
+				triggers[hook_name] = new Hook;
+			});
+			this.triggers = triggers;
+		},
+		add_trigger: function(point, callback){
+			var point = point.toLowerCase();
+			if(this.triggers.hasOwnProperty(point)){
+				this.triggers[point].add(callback)
+			}
+		},
+		call_trigger: function(point, args){
+			point = point.toLowerCase();
+			if(this.triggers.hasOwnProperty(point)){
+				this.triggers[point].exec(args);
+			}
+		}
+	});
+}).call(LDR);
+
 window.onload   = init;
 window.onresize = function(){invoke_hook('WINDOW_RESIZE')};
 
@@ -39,56 +90,8 @@ $.enable_cache(
 	'total_unread_count'
 );
 
-/*
- Hook
-*/
-var Hook = Class.create();
-Hook.extend({
-	initialize: function(){
-		this.callbacks = [];
-	},
-	isHook: true,
-	add: function(f){
-		this.callbacks.push(f)
-	},
-	exec: function(){
-		var args = arguments;
-		this.callbacks.forEach(function(f){
-			isFunction(f) && f.apply(null,args)
-		})
-	},
-	clear: function(){
-		this.callbacks = []
-	}
-});
-
-Class.Trigger = Class.create();
-Class.Trigger.extend({
-	initialize: function(){
-		var points = Array.from(arguments);
-		var triggers = {};
-		points.forEach(function(name){
-			var hook_name = name.toLowerCase();
-			triggers[hook_name] = new Hook;
-		});
-		this.triggers = triggers;
-	},
-	add_trigger: function(point, callback){
-		var point = point.toLowerCase();
-		if(this.triggers.hasOwnProperty(point)){
-			this.triggers[point].add(callback)
-		}
-	},
-	call_trigger: function(point, args){
-		point = point.toLowerCase();
-		if(this.triggers.hasOwnProperty(point)){
-			this.triggers[point].exec(args);
-		}
-	}
-});
-
 // まだ作ってないのもあり。
-LDR.trigger = new Class.Trigger(
+LDR.trigger = new LDR.EventTrigger(
 	// Window LOAD/UNLOAD
 	'AFTER_LOAD','BEFORE_UNLOAD',
 	// Application INIT
