ede542730a72310c4f6427dd9b1dfaf233fa45cb
Yamagishi Kazutoshi <ykzts@desire.sh>
2013-03-20 23:59:49 +0900

    Remove static HTML fragment file

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,11 +1,4 @@
 class ContentsController < ApplicationController
   before_filter :login_required
-
-  def configure
-    render layout:false
-  end
-
-  def guide
-    render layout:false
-  end
+  layout false
 end
