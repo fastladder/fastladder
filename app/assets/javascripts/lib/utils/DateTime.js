f9532332e66524453964b373fb7c818968730439
aerith <kzhr@aerith.sc>
2013-03-24 05:10:41 +0900

    Move functions from common.js to DateTime.js

--- app/assets/javascripts/lib/utils/DateTime.js
+++ app/assets/javascripts/lib/utils/DateTime.js
@@ -7,9 +7,7 @@ function DateTime(time){
 	this.toString = function(){ return [this.ymd(),this.hms()].join(" ")}
 	this.valueOf  = function(){ return this._date - 0 };
 }
-DateTime.now = function(){
-	return new DateTime;
-};
+
 DateTime.prototype = {
 	_update : function(){
 		var dt = this._date;
@@ -27,5 +25,13 @@ DateTime.prototype = {
 	hms : function(sep){
 		sep = arguments.length ? sep : ":";
 		return [this.hour,this.minute,this.second].invoke("zerofill",2).join(sep)
-	}
+	},
+    ymd_jp : function() {
+	    var ymd = [this.year,this.month,this.day].invoke("zerofill",2)
+	    return ymd[0]+"年"+ymd[1]+"月"+ymd[2]+"日";
+    }
+};
+
+DateTime.now = function(){
+	return new DateTime;
 };
