af09879093c07a9a2485f5fa5dfbb27a1de96e2b
Yamagishi Kazutoshi <ykzts@desire.sh>
2013-03-24 16:14:18 +0900

    Use assets pipeline for CSS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Fastladder::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( reader.js share.js subscribe.js)
+  config.assets.precompile += %w( reader.js share.js subscribe.js )
+  config.assets.precompile += %w( reader.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
