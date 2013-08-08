8d34553835b355d16ac163bf43f1eb130aa5c4c7
onk <takafumi.onaka@gmail.com>
2013-08-08 17:13:56 +0900

    remove "mass-assignment error" test

--- spec/models/pin_spec.rb
+++ spec/models/pin_spec.rb
@@ -14,20 +14,6 @@
 require 'spec_helper'
 
 describe Pin do
-  describe 'mass-assignment error' do
-    it 'not raises mass-assignment exception' do
-      expect {
-        Pin.create(link: 'http://la.ma.la/blog/diary_200810292006.htm')
-      }.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end
-
-    it 'not raises mass-assignment exception' do
-      expect {
-        Pin.create(title: '近況')
-      }.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end
-  end
-
   describe ".after_create" do
     it "destroy_over_limit_pins called" do
       pin = FactoryGirl.build(:pin)
