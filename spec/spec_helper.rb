9a6c5c6fd9d33de016a0f07377da07d5d6c92186
Kohei Suzuki <eagletmt@gmail.com>
2014-06-06 23:53:34 +0900

    Remove "require 'rspec/autorun'"
    Requiring `rspec/autorun` when running RSpec via the `rspec` command is
    deprecated.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ ENV["RAILS_ENV"] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/rails'
 
