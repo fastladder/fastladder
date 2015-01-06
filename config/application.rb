395c1a35ef4ccfd7e9f9d8cbf67792f48609d84f
Kohei Suzuki <eagletmt@gmail.com>
2015-01-07 01:37:18 +0900

    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module Fastladder
     # > Cannot precompile i18n-js translations unless environment is initialized.
     # > Please set config.assets.initialize_on_precompile to true.
     config.assets.initialize_on_precompile = true
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
