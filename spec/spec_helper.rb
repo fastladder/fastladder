9bc4e88ccfe58fceed1871ba5398bdbe8971e143
Kohei Suzuki <eagletmt@gmail.com>
2017-01-18 01:05:58 +0900

    rails rails:update
    WebMock configurations should be moved into spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,9 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
