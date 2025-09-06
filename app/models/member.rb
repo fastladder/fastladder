1250f7b9ab9a207b4180a84f491dad646127290e
ssig33 <mail@ssig33.com>
2025-09-06 21:45:38 +0900

    I used the wrong serializer. It should be YAML, not JSON.

--- app/models/member.rb
+++ app/models/member.rb
@@ -21,7 +21,7 @@ require "digest/sha1"
 class Member < ActiveRecord::Base
   # Virtual attribute for the unencrypted password
   attr_accessor :password
-  serialize :config_dump, coder: JSON
+  serialize :config_dump, coder: YAML
 
   has_many :folders
   has_many :subscriptions
