56027615fb0c0828d41eeda5643cac63321df638
ssig33 <mail@ssig33.com>
2025-09-06 11:48:26 +0900

    require libs

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "./lib/string"
+require "./lib/fastladder"
+
 module Fastladder
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
